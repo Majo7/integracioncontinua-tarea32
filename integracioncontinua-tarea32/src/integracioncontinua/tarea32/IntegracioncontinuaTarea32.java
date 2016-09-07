f111642fb69e2438f35915d3a83e50db11d0960a
USUARIO-PC\USUARIO <majitobella_star@hotmail.com>
2016-09-07 03:08:36 -0500

    commit numero one

---
+++ integracioncontinua-tarea32/src/integracioncontinua/tarea32/IntegracioncontinuaTarea32.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integracioncontinua.tarea32;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class IntegracioncontinuaTarea32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Github-- Hola---Soy Majo");
+    }
+    
+}
